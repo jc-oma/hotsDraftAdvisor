1cd046daa6c7968745ed7da15a245ad9a7289e25
Janek Czapowski <janek.czapowski@comdirect.de>
2025-09-19 18:06:15 +0200

    added some more icons from material design to build gradle

--- app/src/main/java/com/example/hotsdraftadviser/composables/menus/MenuMainActivityComposable.kt
+++ app/src/main/java/com/example/hotsdraftadviser/composables/menus/MenuMainActivityComposable.kt
@@ -1,9 +1,13 @@
 package com.example.hotsdraftadviser.composables.menus
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.AccountBox
+import androidx.compose.material.icons.outlined.CompareArrows
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
@@ -18,7 +22,7 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun MenuMainActivityComposable(modifier: Modifier = Modifier, onDisclaymer: () -> Unit, onToggleListMode: () -> Unit) {
-    var expanded by remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(true) }
     Box(
         modifier = modifier
             .padding(16.dp)
@@ -38,8 +42,14 @@ fun MenuMainActivityComposable(modifier: Modifier = Modifier, onDisclaymer: () -
                 text = { Text("Disclaim") },
                 onClick = { onDisclaymer() }
             )
+            // TODO
+            //  drawable/skull_list_24"
             DropdownMenuItem(
-                text = { Text("Toggle List Mode") },
+                text = { Row{
+                    Icon(Icons.AutoMirrored.Filled.List, contentDescription = "List")
+                    Icon(Icons.Outlined.CompareArrows, contentDescription = "List")
+                    Icon(Icons.Outlined.AccountBox, contentDescription = "List")
+                } },
                 onClick = { onToggleListMode() }
             )
         }
