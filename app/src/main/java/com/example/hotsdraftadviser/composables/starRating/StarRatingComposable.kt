bd6ddc426368cfb4b84f04e3a31182cff8c0884f
jc-oma <47916084+jc-oma@users.noreply.github.com>
2025-09-24 14:36:25 +0200

    completed starratingcomposable
    completed starratingcomposable

--- app/src/main/java/com/example/hotsdraftadviser/composables/starRating/StarRatingComposable.kt
+++ app/src/main/java/com/example/hotsdraftadviser/composables/starRating/StarRatingComposable.kt
@@ -1,25 +1,32 @@
 package com.example.hotsdraftadviser.composables.starRating
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun StarRatingComposable(float: Float) {
     val maxStars = 3
-    val filledStars = float * maxStars
+    var filledStars = float * maxStars
     val unfilledStars = maxStars - filledStars
 
-    for (i in 1..filledStars.toInt()) {
-        StarComposable()
-    }
-
-    for (i in 1..unfilledStars.toInt()) {
-        StarComposable()
+    Row {
+        for (i in 1..maxStars) {
+            if (filledStars >= 1) {
+                StarComposable(1f)
+            } else if (unfilledStars < 0) {
+                StarComposable(0f)
+            }
+            else {
+                StarComposable(filledStars)
+            }
+            filledStars -= 1
+        }
     }
 }
 
 @Preview
 @Composable
 fun StarRatingComposablePreview() {
-    StarRatingComposable(float = 0.9f)
+    StarRatingComposable(float = 0.5f)
 }
