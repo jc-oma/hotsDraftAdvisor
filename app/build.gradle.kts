90f4d464a6b0d5e318896c7c47f81b5c34b6fac4
jc-oma <47916084+jc-oma@users.noreply.github.com>
2025-10-11 09:55:14 +0200

    Release Version 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.jcdevelopment.hotsdraftadviser"
         minSdk = 31
         targetSdk = 36
-        versionCode = 8
+        versionCode = 9
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
