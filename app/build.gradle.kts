d14fb4fb440e03d76b034dd2918631fa99403ba3
jc-oma <47916084+jc-oma@users.noreply.github.com>
2025-10-26 06:57:56 +0100

    release version 17
    added korea and changed default language to english

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.jcdevelopment.hotsdraftadviser"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
+        versionCode = 17
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
