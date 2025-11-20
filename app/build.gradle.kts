c2ab650ca290bbe8390ec5eeffc1a79468c3a108
jc-oma <47916084+jc-oma@users.noreply.github.com>
2025-11-20 15:04:43 +0100

    minor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.jcdevelopment.hotsdraftadviser"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "1.0.0"
+        versionCode = 29
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/jcdevelopment/hotsdraftadviser/composables/pickedChamps/ListOfBannedChampItem.kt
+++ app/src/main/java/com/jcdevelopment/hotsdraftadviser/composables/pickedChamps/ListOfBannedChampItem.kt
@@ -21,59 +21,43 @@ import com.jcdevelopment.hotsdraftadviser.dataclasses.exampleChampDataSgtHammer
 
 @Composable
 fun ListOfBannedChampItem(
-    ownBannedChamps: List<ChampData>,
-    theirBannedChamps: List<ChampData>,
+    modifier: Modifier = Modifier,
+    bannedChamps: List<ChampData>,
     removeBan: (Int, TeamSide) -> Unit,
     painter: Painter
 ) {
-    Row {
-        LazyRow {
-            items(ownBannedChamps.size) { i ->
-                Box(modifier = Modifier.fillMaxSize()) {
-                    Image(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(start = 2.dp),
-                        painter = painterResource(Utilitys.mapChampNameToRoundPortraitDrawable(ownBannedChamps[i].ChampName)!!),
-                        contentDescription = theirBannedChamps[i].ChampName
-                    )
-                    Image(
-                        modifier = Modifier.fillMaxSize()
-                            .padding(bottom = 2.dp),
-                        painter = painterResource(R.drawable.frame_ban),
-                        contentDescription = ""
-                    )
-                }
-            }
-        }
-        LazyRow {
-            items(ownBannedChamps.size) { i ->
-                Box(modifier = Modifier.fillMaxSize()) {
-                    Image(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(start = 2.dp),
-                        painter = painterResource(Utilitys.mapChampNameToRoundPortraitDrawable(theirBannedChamps[i].ChampName)!!),
-                        contentDescription = theirBannedChamps[i].ChampName
-                    )
-                    Image(
-                        modifier = Modifier.fillMaxSize()
-                            .padding(bottom = 2.dp),
-                        painter = painterResource(R.drawable.frame_ban),
-                        contentDescription = ""
-                    )
-                }
+    LazyRow(modifier = modifier) {
+        items(bannedChamps.size) { i ->
+            Box(modifier = Modifier.fillMaxSize()) {
+                Image(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(start = 4.3.dp, end = 4.3.dp, bottom = 4.3.dp, top = 3.5.dp),
+                    painter = painterResource(
+                        Utilitys.mapChampNameToRoundPortraitDrawable(
+                            bannedChamps[i].ChampName
+                        )!!
+                    ),
+                    contentDescription = bannedChamps[i].ChampName
+                )
+                Image(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(start = 3.5.dp),
+                    painter = painterResource(R.drawable.frame_ban),
+                    contentDescription = ""
+                )
             }
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true, heightDp = 48)
 @Composable
 private fun ListOfBannedChampItemPreview() {
     ListOfBannedChampItem(
-        ownBannedChamps = listOf(exampleChampDataSgtHammer, exampleChampDataAbathur),
-        theirBannedChamps = listOf(exampleChampDataSgtHammer, exampleChampDataSgtHammer),
+        modifier = Modifier.fillMaxSize(),
+        bannedChamps = listOf(exampleChampDataSgtHammer, exampleChampDataAbathur),
         removeBan = { _, _ -> {} },
         painter = painterResource(
             id = R.drawable.round_portrait_sgthammer
